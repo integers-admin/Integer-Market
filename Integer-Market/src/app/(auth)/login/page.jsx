b4b71d7afc95c24d9dbcc7448c7b7a188229ae52
integers-admin <integers1252@gmail.com>
2026-07-01 09:45:18 +0530

    new-commit

--- Integer-Market/src/app/(auth)/login/page.jsx
+++ Integer-Market/src/app/(auth)/login/page.jsx
@@ -1,14 +1,38 @@
-import Login from '../../../views/auth/Login'
+// import Login from '../../../views/auth/Login'
+
+// export const metadata = {
+//   title: 'Log In',
+//   description: 'Log in to your Integer Market account to access your purchased reports and manage your subscription.',
+//   robots: {
+//     index: false,
+//     follow: false,
+//   },
+// }
+
+// export default function LoginPage() {
+//   return <Login />
+// }
+
+
+
+
+import { Suspense } from "react";
+import Login from "../../../views/auth/Login";
 
 export const metadata = {
-  title: 'Log In',
-  description: 'Log in to your Integer Market account to access your purchased reports and manage your subscription.',
+  title: "Log In",
+  description:
+    "Log in to your Integer Market account to access your purchased reports and manage your subscription.",
   robots: {
     index: false,
     follow: false,
   },
-}
+};
 
 export default function LoginPage() {
-  return <Login />
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Login />
+    </Suspense>
+  );
 }
