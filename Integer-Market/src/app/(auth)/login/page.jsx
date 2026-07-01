8e44b5d6bf0dae76e31d0f6565c248eb3bb6c140
integers-admin <integers1252@gmail.com>
2026-07-01 12:25:35 +0530

    new-commit

--- Integer-Market/src/app/(auth)/login/page.jsx
+++ Integer-Market/src/app/(auth)/login/page.jsx
@@ -16,28 +16,42 @@
 
 
 
+// import { Suspense } from "react";
+// import Login from "../../../views/auth/Login";
+// import { connection } from "next/server";
+
+// export const dynamic = 'force-dynamic';
+
+// export const metadata = {
+//   title: "Log In",
+//   description:
+//     "Log in to your Integer Market account to access your purchased reports and manage your subscription.",
+//   robots: {
+//     index: false,
+//     follow: false,
+//   },
+// };
+
+// export default async function LoginPage() {
+
+//   await connection();
+
+//   return (
+//     <Suspense fallback={<div>Loading...</div>}>
+//       <Login />
+//     </Suspense>
+//   );
+// }
+
+// src/app/(auth)/login/page.js
+"use client";
+
 import { Suspense } from "react";
 import Login from "../../../views/auth/Login";
-import { connection } from "next/server";
 
-export const dynamic = 'force-dynamic';
-
-export const metadata = {
-  title: "Log In",
-  description:
-    "Log in to your Integer Market account to access your purchased reports and manage your subscription.",
-  robots: {
-    index: false,
-    follow: false,
-  },
-};
-
-export default async function LoginPage() {
-
-  await connection();
-
+export default function LoginPage() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<div className="min-h-screen flex items-center justify-center">Loading...</div>}>
       <Login />
     </Suspense>
   );
