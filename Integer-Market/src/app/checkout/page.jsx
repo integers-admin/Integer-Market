769ecc0e774b2a03ca6cfd48d527e816940bf41d
integers-admin <integers1252@gmail.com>
2026-07-01 11:55:24 +0530

    new-commit

--- Integer-Market/src/app/checkout/page.jsx
+++ Integer-Market/src/app/checkout/page.jsx
@@ -38,8 +38,49 @@
 
 
 
+// import { Suspense } from "react";
+
+// const Checkout = dynamic(
+//   () => import("../../../views/Checkout"),
+//   { ssr: false }
+// );
+// // import Checkout from "../../views/Checkout";
+// import { connection } from "next/server";
+
+// export const dynamic = "force-dynamic";
+
+// export const metadata = {
+//   title: "Checkout",
+//   description: "Complete your market research report purchase securely.",
+//   robots: {
+//     index: false,
+//     follow: false,
+//   },
+// };
+
+// export default async function CheckoutPage({ searchParams }) {
+
+//   await connection()
+
+//   const params = await searchParams;
+
+//   return (
+//     <Suspense fallback={<div>Loading...</div>}>
+//       <Checkout reportId={params?.reportId || null} />
+//     </Suspense>
+//   );
+// }
+
+
+
 import { Suspense } from "react";
-import Checkout from "../../views/Checkout";
+import dynamic from "next/dynamic";
+
+const Checkout = dynamic(
+  () => import("../../../views/Checkout"),
+  { ssr: false }
+);
+
 import { connection } from "next/server";
 
 export const dynamic = "force-dynamic";
@@ -54,13 +95,11 @@ export const metadata = {
 };
 
 export default async function CheckoutPage({ searchParams }) {
-
-  await connection()
-
+  await connection();
   const params = await searchParams;
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<div className="min-h-screen flex items-center justify-center">Loading...</div>}>
       <Checkout reportId={params?.reportId || null} />
     </Suspense>
   );
