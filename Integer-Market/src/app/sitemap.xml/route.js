c1dbc5e16bfe892e29886020f786886c1ef02ff5
Integers Insights <integers1252@gmail.com>
2026-07-25 17:26:52 +0530

    new-commit

--- Integer-Market/src/app/sitemap.xml/route.js
+++ Integer-Market/src/app/sitemap.xml/route.js
@@ -5,6 +5,9 @@ export async function GET() {
 
   const xml = await response.text();
 
+  console.log("Response Body:");
+  // console.log(xml);
+
   return new Response(xml, {
     headers: {
       "Content-Type": "application/xml",
